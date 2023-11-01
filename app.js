7f009cd0c62c1f86ad3d347baa2b7bdadc2c94cf
Samar AYADI <ayadisamardev@gmail.com>
2023-11-01 19:10:58 +0100

    Connect project to MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", {root: __dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+mongoose.connect("mongodb+srv://samardev:12345@cluster0.tblkyye.mongodb.net/all-data?retryWrites=true&w=majority").then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+   })
+   .catch((err) => {
+     console.log(err);
+   });

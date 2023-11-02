07b6e5c2240a1b667fa534098db73024187032c9
Samar AYADI <ayadisamardev@gmail.com>
2023-11-02 02:58:30 +0100

    send data to database

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 
+app.use(express.urlencoded({ extended: true }));
+const Article = require("./models/articleSchema.js");
+
+
+
+
+ 
+
+
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", {root: __dirname})
 })
 
+app.post("/all-articles", (req, res) => {
+  Article
+    .create(req.body)
+    .then( result => {
+      res.redirect("/");
+    })
+    .catch( err => {
+      console.log(err);
+    });
+});
 
 mongoose.connect("mongodb+srv://samardev:12345@cluster0.tblkyye.mongodb.net/all-data?retryWrites=true&w=majority").then(() => {
     app.listen(port, () => {
